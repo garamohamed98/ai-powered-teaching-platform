43dcb6eca35cb9b88c4d0322b0bbad696f138a72
Mohamed Gara <garamohamed9@gmail.com>
2026-06-20 11:55:01 +0100

    fix: fixing env properties naming

--- ai-teaching-platform-backend/src/main/java/com/mohamedgara/ai_teaching_platform/AI/config/GeminiProperties.java
+++ ai-teaching-platform-backend/src/main/java/com/mohamedgara/ai_teaching_platform/AI/config/GeminiProperties.java
@@ -1,10 +1,14 @@
 package com.mohamedgara.ai_teaching_platform.AI.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.Name;
 
 @ConfigurationProperties(prefix = "gemini")
 public record GeminiProperties(
+        @Name("api-key")
         String apiKey,
+        @Name("model")
         String model,
+        @Name("base-url")
         String baseUrl
 ) {}
